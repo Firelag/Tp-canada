501bc704bd84992b1f913946afb3ecd24a47a910
Firelag <jordan.vincent085@gmail.com>
2022-12-14 09:10:40 +0100

    projet

---
+++ About.php
@@ -0,0 +1,14 @@
+<?php get_header();
+//Template Name: About
+?>
+<?php $thumb = get_the_post_thumbnail_url(); ?>
+<div class="image-class" style="background-image: url('<?php echo $thumb;?>')"></div>
+
+<img src="<?php echo $thumb;?>" alt="">
+
+<body>
+   
+    <section class="section1">
+        <h2>Les régions du canada</h2>
+    </section>
+</body>
